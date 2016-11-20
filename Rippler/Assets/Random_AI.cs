f26e3ea88f92ff4912176f09d3a847f984229d7b
TerminusLi <thomasli35769@gmail.com>
2016-11-20 14:27:51 -0500

    Add files via upload
    updated

--- Rippler/Assets/Random_AI.cs
+++ Rippler/Assets/Random_AI.cs
@@ -12,12 +12,12 @@ public class Random_AI : MonoBehaviour
     private int numNodes;
     private bool reachedNewNode;
     private float totalTravel;
+    public bool interactingWithPlayer;
 
 
     // Use this for initialization
     void Start()
     {
-        Debug.Log("I got here");
         numNodes = nodes.Length;
         int r = (int)Random.Range(0, numNodes);
         curNode = nodes[r];
@@ -25,32 +25,34 @@ public class Random_AI : MonoBehaviour
         transform.position = startPos;
         reachedNewNode = true;
         totalTravel = 0;
+        interactingWithPlayer = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (reachedNewNode == true)
+        if (interactingWithPlayer == false)
         {
-            Debug.Log("Now I should choose a new next node.");
-            curNode = nextNode;
-            nextNode = chooseNextNode();
-            reachedNewNode = false;
-        }
-        else
-        {
-            Debug.Log("Better move to the next node.");
-            float difX = nextNode.transform.position.x - curNode.transform.position.x;
-            float difY = nextNode.transform.position.y - curNode.transform.position.y;
-            float distToReach = Mathf.Sqrt(difX * difX + difY * difY);
-            Vector2 move = new Vector2((difX/distToReach) * walkSpeed / 100, (difY/distToReach) * walkSpeed / 100);
-            transform.Translate(move);
-            totalTravel += move.magnitude;
-            if (totalTravel >= distToReach)
+            if (reachedNewNode == true)
             {
-                transform.position = nextNode.transform.position;
-                totalTravel = 0;
-                reachedNewNode = true;
+                curNode = nextNode;
+                nextNode = chooseNextNode();
+                reachedNewNode = false;
+            }
+            else
+            {
+                float difX = nextNode.transform.position.x - curNode.transform.position.x;
+                float difY = nextNode.transform.position.y - curNode.transform.position.y;
+                float distToReach = Mathf.Sqrt(difX * difX + difY * difY);
+                Vector2 move = new Vector2((difX / distToReach) * walkSpeed / 100, (difY / distToReach) * walkSpeed / 100);
+                transform.Translate(move);
+                totalTravel += move.magnitude;
+                if (totalTravel >= distToReach)
+                {
+                    transform.position = nextNode.transform.position;
+                    totalTravel = 0;
+                    reachedNewNode = true;
+                }
             }
         }
     }
@@ -75,7 +77,6 @@ public class Random_AI : MonoBehaviour
 
     GameObject chooseNextNode()
     {
-        Debug.Log("Choosing a node");
         while (true)
         {
             int r = (int)Random.Range(0, numNodes);
